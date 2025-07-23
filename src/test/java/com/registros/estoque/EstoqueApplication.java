c6e1f83fe121010f2d95bc98ad45342647a5967a
🌑João Fogaça (SC/ALUNO SATC)🌑 <joao.1824@alunosatc.edu.br>
2025-07-23 17:00:37 -0300

    configurando algumas dependencias

---
+++ src/test/java/com/registros/estoque/EstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.registros.estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class EstoqueApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/registros/estoque/ItemController/ItemController.java
@@ -0,0 +1,14 @@
+package com.registros.estoque.ItemController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("item")
+public class ItemController {
+    @GetMapping
+    public void getAll(){
+
+    }
+}
